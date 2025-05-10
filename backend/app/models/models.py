4d2f7c590245d4987b044ecd52a46a0190f3e5ba
Cliiive <sasowski@outlook.de>
2025-05-10 22:27:31 +0200

    DB/Models: add second table for encryption keys & add colums to the file model

--- backend/app/models/models.py
+++ backend/app/models/models.py
@@ -5,23 +5,31 @@ from sqlalchemy import Column, Integer, LargeBinary
 Base = declarative_base()
 
 # Define a model to store the AES key
-class EncryptionKey(Base):
-    __tablename__ = 'encryption_keys'
+class SymmetricalKey(Base):
+    __tablename__ = 'symmetrical_keys'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     file_id = Column(Integer, ForeignKey('files.id'), nullable=False)
+    key = Column(LargeBinary, nullable=False)
+
+class PublicKey(Base):
+    __tablename__ = 'public_keys'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
     active = Column(Boolean, nullable=False)
     key = Column(LargeBinary, nullable=False)
 
 class Files(Base):
     __tablename__ = 'files'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     path = Column(VARCHAR(255), nullable=False)
+    file_name = Column(VARCHAR(255), nullable=False)
+    content_type = Column(VARCHAR(100), nullable=False)
 
 class Users(Base):
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     alias = Column(VARCHAR(255), nullable=False)
