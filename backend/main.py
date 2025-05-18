1353392afc3a28d3b95c4689d9043f8db52c88b3
Cliiive <sasowski@outlook.de>
2025-05-18 21:13:31 +0200

    Journalist/API: add script + api route to fetch new files

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.user import router as user_router
 from app.api.v1.upload import router as upload_router
 from app.api.v1.auth import router as auth_router
+from app.api.v1.download import router as download_router
 from app.api.v1.rsa_upload import router as public_key_router
 from app.core.config import settings
 from app.db.session import init_db, create_admin_account
@@ -36,3 +37,4 @@ app.include_router(user_router, prefix=settings.API_PREFIX + "/user")
 app.include_router(upload_router, prefix=settings.API_PREFIX + "/upload")
 app.include_router(auth_router, prefix=settings.API_PREFIX + "/auth")
 app.include_router(public_key_router, prefix=settings.API_PREFIX + "/publickey")
+app.include_router(download_router, prefix=settings.API_PREFIX + "/download")
